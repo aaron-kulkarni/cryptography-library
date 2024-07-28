ebd88c796ce2b211c821f7a32533d3af964347f8
aaronk2711 <aaronkulkarni@utexas.edu>
2024-07-28 17:50:38 -0400

    added sbox

---
+++ src/aes/aes.rs
@@ -0,0 +1,14 @@
+fn encrypt() {
+	//for every block of the plaintext input
+		// key generation: generate a set of round keys from a secret key
+		//addition of first round key to input
+		//series of rounds, defined by length of secret key
+			//1. subsitute bytes
+			//2. shift rows
+			//3. mix columns
+			//4. adding round key
+		//final round: skip mix columns step
+}
+fn decrypt() {
+
+}
