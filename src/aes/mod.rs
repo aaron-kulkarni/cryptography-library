5240640d813b78bcb278ce867a41f32be6a82c57
aaron-kulkarni <aaron.d.kulkarni@gmail.com>
2024-08-21 12:19:50 -0400

    refactoring cli

--- src/aes/mod.rs
+++ src/aes/mod.rs
@@ -1,3 +1,4 @@
+pub mod cli;
 pub mod decrypt;
 pub mod encrypt;
 pub mod utils;
--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,53 @@
-use std::process;
 mod aes;
-use aes::utils::AESConfig;
 use aes::utils::KeyLength;
 mod cli;
-use std::fs::File;
-use std::io::prelude::*;
+use dialoguer::Select;
+
+#[derive(Debug)]
+enum Algorithm {
+    AES,
+    SHA,
+    RSA,
+}
 
 fn main() {
-    let config = match cli::init_aes_config() {
-        Ok(x) => x,
-        //TODO: exit gracefully
-        Err(_) => process::exit(8),
+    let algo = init_algo_config();
+
+    match algo {
+        Algorithm::AES => {
+            let config = match aes::cli::init_aes_config() {
+                Ok(x) => x,
+                //TODO: exit gracefully
+                Err(_) => return,
+            };
+            aes::cli::run_aes(config);
+        }
+        Algorithm::SHA => {
+            println!("Not implemented yet!")
+        }
+        Algorithm::RSA => {
+            println!("Not implemented yet!")
+        }
+    }
+}
+
+fn init_algo_config() -> Algorithm {
+    println!("Welcome. What encryption algorithm would you like to use?");
+
+    let algo = match Select::new()
+        .with_prompt("Do you want to encrypt or decrypt a message?")
+        .default(0)
+        .item("AES")
+        .item("SHA")
+        .item("RSA")
+        .interact()
+        .unwrap()
+    {
+        0 => Algorithm::AES,
+        1 => Algorithm::SHA,
+        2 => Algorithm::RSA,
+        _ => panic!("I don't think this is possible"),
     };
 
-    if config.encrypt {
-        println!("Encrypting...");
-        let key_clone = config.key.clone();
-        let mut text_clone = config.base_bytes.clone();
-        let encrypted_bytes = aes::encrypt::encrypt(&mut text_clone, config.key);
-
-        let mut file = match File::create("mykey.txt") {
-            Ok(f) => f,
-            Err(_) => {
-                println!("Could not create mykey.txt.");
-                process::exit(3);
-            }
-        };
-
-        if let Err(_) = file.write_all(&key_clone) {
-            println!("Failed to write to file mykey.txt.");
-            process::exit(4);
-        }
-
-        let mut file2 = match File::create("mymsg.txt") {
-            Ok(f) => f,
-            Err(_) => {
-                println!("Could not create mymsg.txt");
-                process::exit(5);
-            }
-        };
-
-        if let Err(_) = file2.write_all(&encrypted_bytes) {
-            println!("Failed to write to mymsg.txt.");
-            process::exit(6);
-        }
-
-        println!(
-            "The key for this encryption has been saved in my_key.txt.
-            The encrypted message has been saved in my_msg.txt.
-            Doing another encryption operation will overwrite both these files."
-        );
-
-        return;
-    } else {
-        println!("Decrypting...");
-
-        let decrypted_string = aes::decrypt::decrypt(config.base_bytes, config.key);
-        println!("This is the decrypted string: {:?}", decrypted_string);
-        return;
-    }
+    return algo;
 }
